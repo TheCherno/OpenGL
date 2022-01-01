09d92d515977d9e520fdfa6fab434fe59e8e0c33
nullptr <eliesbertran@gmail.com>
2022-01-01 20:11:54 +0100

    Cleaned unused OpenGL-Core code

--- OpenGL-Core/src/GLCore/Util/Shader.h
+++ OpenGL-Core/src/GLCore/Util/Shader.h
@@ -6,21 +6,6 @@
 
 namespace GLCore::Utils {
 
-	class Shader
-	{
-	public:
-		~Shader();
-
-		GLuint GetRendererID() { return m_RendererID; }
-
-		static Shader* FromGLSLTextFiles(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
-	private:
-		Shader() = default;
-
-		void LoadFromGLSLTextFiles(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
-		GLuint CompileShader(GLenum type, const std::string& source);
-	private:
-		GLuint m_RendererID;
-	};
+	GLuint CreateShader(const std::string& source);
 
 }
