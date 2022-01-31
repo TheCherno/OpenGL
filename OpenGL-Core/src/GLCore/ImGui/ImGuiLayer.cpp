c38f88dfb8f3efdf2521a6b33a2529b64803a438
nullptr <eliesbertran@gmail.com>
2022-01-31 12:00:17 +0100

    Updated imgui's glsl version

--- OpenGL-Core/src/GLCore/ImGui/ImGuiLayer.cpp
+++ OpenGL-Core/src/GLCore/ImGui/ImGuiLayer.cpp
@@ -44,7 +44,7 @@ namespace GLCore {
 
 		// Setup Platform/Renderer bindings
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
-		ImGui_ImplOpenGL3_Init("#version 410");
+		ImGui_ImplOpenGL3_Init("#version 400");
 	}
 
 	void ImGuiLayer::OnDetach()
