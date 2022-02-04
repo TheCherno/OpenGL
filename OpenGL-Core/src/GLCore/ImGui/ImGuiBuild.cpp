90ebdc1d3b9887c239a72d7986786afd85f9bf02
nullptr <eliesbertran@gmail.com>
2022-02-04 14:09:55 +0100

    Removed old OpenGL-Core

--- OpenGL-Core/src/GLCore/ImGui/ImGuiBuild.cpp
+++
@@ -1,5 +0,0 @@
-#include "glpch.h"
-
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include "backends/imgui_impl_opengl3.cpp"
-#include "backends/imgui_impl_glfw.cpp"
--- OpenGL-Core/src/GLCoreUtils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Utility header file - include into application for access to utility classes/functions
-
-#include "GLCore/Util/Shader.h"
-#include "GLCore/Util/OpenGLDebug.h"
-#include "GLCore/Util/TextureUtils.h"
